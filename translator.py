76c1eb715a7cc5285dc107ebc80c2cb9ff419af5
Alex Stolz <alex.stolz@ebusiness-unibw.org>
2011-11-08 08:54:33 +0100

    added additional vocabularies

--- translator.py
+++ translator.py
@@ -28,6 +28,7 @@ known_vocabs = {
     "owl": "http://www.w3.org/2002/07/owl#",
     "dbpedia": "http://dbpedia.org/resource/",
     "dc": "http://purl.org/dc/elements/1.1/",
+    "dcterms": "http://purl.org/dc/terms/",
     "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
     "gr": "http://purl.org/goodrelations/v1#",
     "foaf": "http://xmlns.com/foaf/0.1/",
@@ -39,7 +40,25 @@ known_vocabs = {
     "fab": "http://purl.org/fab/ns#",
     "xro": "http://www.stalsoft.com/ontologies/xro/ns#",
     "xhv": "http://www.w3.org/1999/xhtml/vocab#",
-    "s": "http://schema.org/"
+    "s": "http://schema.org/",
+    "grddl": "http://www.w3.org/2003/g/data-view#",
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "rdfa": "http://www.w3.org/ns/rdfa#",
+    "rif": "http://www.w3.org/2007/rif#",
+    "skos": "http://www.w3.org/2004/02/skos/core#",
+    "skosxl": "http://www.w3.org/2008/05/skos-xl#",
+    "wdr": "http://www.w3.org/2007/05/powder#",
+    "void": "http://rdfs.org/ns/void#",
+    "wdsr": "http://www.w3.org/2007/05/powder-s#",
+    "xml": "http://www.w3.org/XML/1998/namespace",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
+    "cc": "http://creativecommons.org/ns#",
+    "ctag": "http://commontag.org/ns#",
+    "ical": "http://www.w3.org/2002/12/cal/icaltzd#",
+    "og": "http://ogp.me/ns#",
+    "rev": "http://purl.org/stuff/rev#",
+    "sioc": "http://rdfs.org/sioc/ns#",
+    "v": "http://rdf.data-vocabulary.org/#"
 }
 
 def createSnippet(inputrdf, format="rdfa"):
